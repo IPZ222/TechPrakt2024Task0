22243717805e714d80c0a5bce67e53e2b3b1c3c4
Ilya Ilyin <ipz222_ii@student.ztu.edu.ua>
2024-01-28 14:41:33 +0200

    Added c# console solution and redacted README and gitignore

---
+++ prakttest/prakttest/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a + b;
+Console.WriteLine($"Sum of two numbers: {c}");
+
